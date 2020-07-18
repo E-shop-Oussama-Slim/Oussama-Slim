ab3b2771e7c3e7cd384537ff222018a05585bd0a
Slim-Kasraoui <cabinet.kesraoui@hotmail.com>
2020-07-18 09:33:25 +0100

    refactored

---
+++ new/basket.js
@@ -0,0 +1,55 @@
+/*var total = 0;
+function totalToPay(arr){
+	total += arr[i].price
+	document.getElementById("").value = total;
+} */  
+//-----------------------------------------------------
+document.getElementById("home").onclick = function () {
+        location.href = "home.html";
+    };
+
+//--------------------------------------------------------
+function empty(){
+	if(document.getElementById("first").value === "" || document.getElementById("last").value === "" || document.getElementById("adress").value === "" || document.getElementById("state").value === "" || document.getElementById("phone").value === "" || document.getElementById("email").value === ""){
+		alert("You should fill all element");
+	}
+	return true;
+};
+
+function reset() { 
+        document.getElementById("output").value = 0;
+};
+//-----------------------------------------------------
+function del(){
+	$("#carte").empty();
+}
+function ph(){
+	var check = /^\d{8}$/;
+	var ph = document.getElementById("phone").value;
+	if(ph.length !== 8 || !ph.match(check)){
+			alert("plz enter the reight phone number");
+	}
+	return true;
+};
+//-----------------------------------------------------------
+function email(){
+	var em = document.getElementById("email").value;
+	 if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(em)){
+	 	return true
+	 }
+	 alert ("wrong email")
+}
+//-------------------------------------------------------------
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "slim.oussama.ch5secondproject@gmail.com",
+	Password : "23472929",
+	To : 'document.getElementById("email").values',
+	From : "slim.oussama.ch5secondproject@gmail.com",
+	Subject : "email subject",
+	Body : "email body",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
